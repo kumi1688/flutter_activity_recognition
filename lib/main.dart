2658fd57cf480b430aedb601a0739820c9c7e78b
kang sung hwan <kumi1688@gmail.com>
2020-07-16 12:13:01 +0900

    [Iteration1] make project to test activity recognition

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootPage());
+}
+
+class RootPage extends StatefulWidget {
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
