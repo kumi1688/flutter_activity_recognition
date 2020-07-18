c1867e81c26e9497d7bff1132278bbd35ef270cd
kang sung hwan <kumi1688@gmail.com>
2020-07-18 22:29:25 +0900

    [Iteration1] add accelerometer.dart stream in walking_rocognition_page

--- lib/page/recognition_walking.dart
+++ lib/page/recognition_walking.dart
@@ -1,19 +1,25 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_activity_recognition/sensor/accelerometer.dart';
 import 'package:flutter_activity_recognition/sensor/pedometer.dart';
+import 'package:sensors/sensors.dart';
+
 
 class WalkingRecognitionPage extends StatefulWidget {
   @override
   _WalkingRecognitionPageState createState() => _WalkingRecognitionPageState();
 }
 
-final pedometerBloc = new PedometerBloc();
-
 class _WalkingRecognitionPageState extends State<WalkingRecognitionPage> {
+  var pedometerBloc;
+  var accelerometerBloc;
+
   @override
   void initState(){
     super.initState();
+    pedometerBloc = new PedometerBloc();
+    accelerometerBloc = new AccelerometerBloc();
   }
 
   @override
@@ -25,7 +31,9 @@ class _WalkingRecognitionPageState extends State<WalkingRecognitionPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             pedometerWidget(),
-
+            accelerometerWidget(),
+            userAccelerometerWidget(),
+            gyroscopeWidget()
           ],
         ),
       )
@@ -33,19 +41,58 @@ class _WalkingRecognitionPageState extends State<WalkingRecognitionPage> {
   }
 
   Widget pedometerWidget(){
-    return Center(
-        child: StreamBuilder<int>(
+    return StreamBuilder<int>(
             stream: pedometerBloc.pedometer,
             initialData: 0,
             builder: (context, snapshot){
               if(snapshot.hasData){
-                return Text('${snapshot.data - pedometerBloc.pedometerInitialValue}', style: TextStyle(fontSize: 30));
+                return Text('${snapshot.data - pedometerBloc.pedometerInitialValue}', style: TextStyle(fontSize: 20));
               } else {
-                return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 30));
+                return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 20));
               }
             }
-        )
+        );
+  }
+
+  Widget accelerometerWidget(){
+    return StreamBuilder<List<String>>(
+        stream: accelerometerBloc.accelerometerValue,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return Text('가속도 ${snapshot.data}', style: TextStyle(fontSize: 20));
+          } else {
+            return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 20));
+          }
+        }
     );
   }
+
+  Widget userAccelerometerWidget(){
+    return StreamBuilder<List<String>>(
+        stream: accelerometerBloc.userAccelerometerValue,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return Text('가속도(중력x) ${snapshot.data}', style: TextStyle(fontSize: 20));
+          } else {
+            return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 20));
+          }
+        }
+    );
+  }
+
+  Widget gyroscopeWidget(){
+    return StreamBuilder<List<String>>(
+        stream: accelerometerBloc.gyroscopeValue,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return Text('평형계 ${snapshot.data}', style: TextStyle(fontSize: 20));
+          } else {
+            return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 20));
+          }
+        }
+    );
+  }
+
+
 }
 
---
+++ lib/sensor/accelerometer.dart
@@ -0,0 +1,46 @@
+
+import 'package:rxdart/rxdart.dart';
+import 'package:sensors/sensors.dart';
+
+class AccelerometerBloc{
+  List<String> _accelerometerValues;
+  List<String> _userAccelerometerValues;
+  List<String> _gyroscopeValues;
+
+  final _accelerometerSubject = BehaviorSubject<List<String>>();
+  final _userAccelerometerSubject = BehaviorSubject<List<String>>();
+  final _gyroscopeSubject = BehaviorSubject<List<String>>();
+
+  AccelerometerBloc(){
+    startListening();
+  }
+
+  void startListening(){
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      _accelerometerValues = toListString(toListDouble(event));
+      _accelerometerSubject.add(_accelerometerValues);
+    });
+    gyroscopeEvents.listen((event) {
+      _gyroscopeValues = toListString(toListDouble(event));
+      _gyroscopeSubject.add(_gyroscopeValues);
+    });
+    userAccelerometerEvents.listen((event) {
+      _userAccelerometerValues = toListString(toListDouble(event));
+      _userAccelerometerSubject.add(_userAccelerometerValues);
+    });
+  }
+
+  List<double> toListDouble(event){
+    List<double> accelerometerValues = <double>[event.x, event.y, event.z];
+    return accelerometerValues;
+  }
+
+  List<String> toListString(List<double> list){
+    List<String> accelerometerString = list?.map((double v) => v.toStringAsFixed(2))?.toList();
+    return accelerometerString;
+  }
+
+  Stream<List<String>> get accelerometerValue => _accelerometerSubject.stream;
+  Stream<List<String>> get gyroscopeValue => _gyroscopeSubject.stream;
+  Stream<List<String>> get userAccelerometerValue => _userAccelerometerSubject.stream;
+}
