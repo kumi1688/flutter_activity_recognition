c33d00fd46feadc69bc661e66b0a58cb377f5dcd
kang sung hwan <kumi1688@gmail.com>
2020-07-18 16:53:06 +0900

    [Iteration1] add pedometer stream in walking_rocognition_page

--- lib/page/recognition_walking.dart
+++ lib/page/recognition_walking.dart
@@ -1,18 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_activity_recognition/sensor/pedometer.dart';
 
 class WalkingRecognitionPage extends StatefulWidget {
   @override
   _WalkingRecognitionPageState createState() => _WalkingRecognitionPageState();
 }
 
-class _WalkingRecognitionPageState extends State<WalkingRecognitionPage> {
+final pedometerBloc = new PedometerBloc();
 
+class _WalkingRecognitionPageState extends State<WalkingRecognitionPage> {
+  @override
+  void initState(){
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('걷기 측정하기')),
-      body: Container()
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            pedometerWidget(),
+
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget pedometerWidget(){
+    return Center(
+        child: StreamBuilder<int>(
+            stream: pedometerBloc.pedometer,
+            initialData: 0,
+            builder: (context, snapshot){
+              if(snapshot.hasData){
+                return Text('${snapshot.data - pedometerBloc.pedometerInitialValue}', style: TextStyle(fontSize: 30));
+              } else {
+                return Text('현재 데이터 가져올 수 없음', style: TextStyle(fontSize: 30));
+              }
+            }
+        )
     );
   }
 }
+
---
+++ lib/sensor/pedometer.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+import 'package:pedometer/pedometer.dart';
+
+class PedometerBloc {
+  final _pedometerSubject = BehaviorSubject<int>();
+  Pedometer _pedometer;
+  int _pedometerInitialValue;
+
+  void startListening() {
+    _pedometer = new Pedometer();
+    _pedometer.pedometerStream.listen(_onData,
+        onError: _onError, onDone: _onDone, cancelOnError: true);
+    _pedometerSubject.addStream(_pedometer.pedometerStream);
+  }
+
+//  void stopListening() {
+//    _subscription.cancel();
+//  }
+
+  void _onData(int newValue) async {
+    if(_pedometerInitialValue == -1){
+      _pedometerInitialValue = newValue;
+    }
+  }
+
+  void _onDone() => print("Finished pedometer tracking");
+  void _onError(error) => print("Flutter Pedometer Error: $error");
+
+  PedometerBloc () {
+      _pedometerInitialValue = -1;
+      startListening();
+  }
+
+  Stream<int> get pedometer => _pedometerSubject.stream;
+  int get pedometerInitialValue => _pedometerInitialValue;
+}
